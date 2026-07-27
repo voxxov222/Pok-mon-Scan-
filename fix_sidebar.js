2f1b2d8481742f771a0d5eb388f5478235f2c2e3
DAPP DESTROYER <freelancealot@cyberdude.com>
2026-07-27 07:53:11 -0600

    segment src/App.tsx Added stylus src/index.css Modified segment src/main.tsx Modified draft README.md Deleted segment fix_sidebar.js Deleted segment fix_sidebar.mjs Deleted segment fix_sidebar2.js Deleted segment fix_sidebar_ts.mjs Deleted segment src/PokeVaultApp.tsx Deleted segment src/vca/App.tsx Deleted image src/vca/assets/images/vca_breach_1784577622387.jpg Deleted image src/vca/assets/images/vca_diagnostics_1784577608909.jpg Deleted image src/vca/assets/images/vca_infographic_1784684822713.jpg Deleted image src/vca/assets/images/vca_overview_1784577595886.jpg Deleted image src/vca/assets/images/vca_slab_new_1784695503544.jpg Deleted segment src/vca/components/AccountView.tsx Deleted segment src/vca/components/AddCardModal.tsx Deleted segment src/vca/components/Auth.tsx Deleted segment src/vca/components/BlueprintArtifact.tsx Deleted segment src/vca/components/CertificateModal.tsx Deleted segment src/vca/components/

--- fix_sidebar.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/vca/components/Sidebar.tsx', 'utf-8');
-code = code.replace(/\{ @ts-ignore \}\n\s*\{item\.badge \{item\.badge \&\& \(\{item\.badge \&\& \( \(/g, '{/* @ts-ignore */}\n                {item.badge && (');
-fs.writeFileSync('src/vca/components/Sidebar.tsx', code);
--- fix_sidebar2.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-let code = fs.readFileSync('src/vca/components/Sidebar.tsx', 'utf-8');
-code = code.replace(/<Sparkles,\s*Camera className="w-5 h-5 text-white" \/>/g, '<Sparkles className="w-5 h-5 text-white" />');
-fs.writeFileSync('src/vca/components/Sidebar.tsx', code);
